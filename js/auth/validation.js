be8c508c5a85cd569051ed78c951f94848abe053
Tifaine <tifaine.hiaume@gmail.com>
2026-05-25 11:34:43 +0200

    done validation.js

---
+++ js/auth/validation.js
@@ -0,0 +1,21 @@
+/*Fonctions partagées avec connexion et inscription*/
+
+/*affiche une erreur sous un champ*/
+function montrerErreur(input, erreurId) {
+    input.classList.remove('valide');
+    input.classList.add('erreur-champ');
+    document.getElementById(erreurId).style.display = 'block';
+}
+
+/*affiche la validation d'un champ*/
+function montrerValide(input, erreurId) {
+    input.classList.remove('erreur-champ');
+    input.classList.add('valide');
+    document.getElementById(erreurId).style.display = 'none';
+}
+
+/*reset les champs et les erreurs*/
+function reinitialiser(input, erreurId) {
+    input.classList.remove('valide', 'erreurId');
+    document.getElementById(erreurId).style.display = 'none';
+}
