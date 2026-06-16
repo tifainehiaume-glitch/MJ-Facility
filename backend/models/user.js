9649f5b609295fc6ea6c7877ccd9f5582a1fbb01
Tifaine <tifaine.hiaume@gmail.com>
2026-06-16 14:20:47 +0200

    create user.js

---
+++ backend/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    pseudo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['joueur', 'admin'],
+        default: 'joueur'
+    },
+    aAcces: {
+        type: Boolean,
+        default: false
+    },
+    fiche:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Fiche',
+        default: null
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
