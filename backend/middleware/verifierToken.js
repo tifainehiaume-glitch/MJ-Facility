cb02b5b6876bfb2b4ffc3f1013310c50803e3ca5
Tifaine <tifaine.hiaume@gmail.com>
2026-06-16 15:41:24 +0200

    create verifierToken.js

---
+++ backend/middleware/verifierToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+function verifierToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token manquant' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+
+    } catch(error) {
+        return res.status(401).json({ message: 'Token invalide ou expiré' });
+    }
+}
+
+module.exports = verifierToken;
