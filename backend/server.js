96bb03a42b1b01c0b3c6aff9a14763c9c63218ca
Tifaine <tifaine.hiaume@gmail.com>
2026-06-17 23:45:55 +0200

    update express.static and added /frontend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../'));
+app.use(express.static('/frontend'));
 
 app.get('/test', (req, res) => {
     res.json({ message: 'Le serveur fonctionne !' });
